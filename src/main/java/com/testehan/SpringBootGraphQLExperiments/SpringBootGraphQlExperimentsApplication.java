e0db9c6098e6f13a6e647f9484768bd83e862983
DanTestehan <tdan89@yahoo.com>
2024-09-04 20:20:20 +0100

    initial commit

---
+++ src/main/java/com/testehan/SpringBootGraphQLExperiments/SpringBootGraphQlExperimentsApplication.java
@@ -0,0 +1,13 @@
+package com.testehan.SpringBootGraphQLExperiments;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootGraphQlExperimentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootGraphQlExperimentsApplication.class, args);
+	}
+
+}
